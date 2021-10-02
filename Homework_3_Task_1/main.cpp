40a7c9548c547f4841cc9638e5a6de524ef458a9
Евгений Пушнин <jack777321444@mail.ru>
2021-10-02 16:26:44 +0300

    Определения количества цифр в числе

---
+++ Homework_3_Task_1/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Дано натуральное число N. Составить программу для определения количества цифр в этом числе.
+
+int main()
+{
+    long long int N;
+    int i;
+
+    printf("Please enter N: ");
+    scanf("%lld", &N);
+
+    for(i = 1; ; ++i)
+    {
+        N/=10;
+        if(N<1)
+        {
+            break;
+        }
+    }
+
+    printf("The number N contains %d digits", i);
+
+    return 0;
+}
