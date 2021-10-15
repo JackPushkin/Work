7121f69b65aa0db5a09d4b5c783d6374af379ae6
Евгений Пушнин <jack777321444@mail.ru>
2021-10-15 22:32:04 +0300

    // Задача на одномерный массив №2

---
+++ Homework_4_Task_2/main.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Определить местоположение элементов массива А (30), не встречающихся в массиве В (15)
+
+int main()
+{
+    srand(time(nullptr));
+
+    // Создаем массивы
+
+    const int SIZE = 30;
+    int array[SIZE];
+    int array2[SIZE/2];
+
+    for(int i = 0; i < SIZE; ++i) // Заполняем массив случайными числами
+    {
+        array[i] = rand() % 50;
+        array2[i] = rand() % 25;
+    }
+
+    for(int i = 0; i < SIZE; ++i) // Вывод первого массива
+    {
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+
+    for(int i = 0; i < SIZE/2; ++i) // Вывод второго массива
+    {
+        printf("%d ", array2[i]);
+    }
+
+    printf("\n\n");
+
+    // Находим индексы элементов массива array[], не встречающихся в массиве array2[]. Вывод индексов в консоль
+
+    int j;
+    for(int i = 0; i < SIZE; ++i)
+    {
+        for(j = 0; j < SIZE/2; ++j)
+        {
+            if(array[i]==array2[j])
+            {
+                break;
+            }
+            if(j == SIZE/2 - 1)
+            {
+                printf("%d ", i);
+            }
+        }
+    }
+
+    return 0;
+}
