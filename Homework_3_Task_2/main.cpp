d0cfe15c88420ef8149c36e3a07b45da615230f5
Евгений Пушнин <jack777321444@mail.ru>
2021-10-22 23:55:44 +0300

    Исп. ф-цию fmin Добавлен unsigned

--- Homework_3_Task_2/main.cpp
+++ Homework_3_Task_2/main.cpp
@@ -1,21 +1,22 @@
 #include <stdio.h>
+#include <math.h>
 
 // Найти наименьший общий делитель трех натуральных чисел
 // (1 будет считаться наименьшим общим делителем только в том случае, когда других общих делителей у заданных чисел нет).
 
 int main()
 {
-    int first_num;
-    int second_num;
-    int third_num;
+    unsigned int first_num;
+    unsigned int second_num;
+    unsigned int third_num;
     int min_num;
     int min_divisor = 1;
 
     printf("Please enter three numbers: \n");
-    scanf("%d%d%d", &first_num, &second_num, &third_num);
+    scanf("%u%u%u", &first_num, &second_num, &third_num);
 
-    min_num = first_num < second_num ? first_num : second_num;
-    min_num = min_num < third_num ? min_num : third_num;
+    min_num = fmin(first_num, second_num);
+    min_num = fmin(min_num, third_num);
 
     for(int i = min_num; i >= 2 ; --i)
     {
